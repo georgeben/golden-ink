4a82679d2d0a6774d188a98afc30302b19d64054
George Benjamin <msdcconnect@gmail.com>
2020-02-26 22:11:50 +0100

    create markup for sign in

---
+++ src/views/SignIn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Sign in</h1>
+    <GoogleSignInButton buttonText='Continue with Google' />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import GoogleSignInButton from '@/components/Auth/GoogleSignInButton.vue';
+
+@Component({
+  components: {
+    GoogleSignInButton,
+  }
+})
+export default class SignIn extends Vue{
+
+}
+</script>
+
+<style>
+
+</style>
