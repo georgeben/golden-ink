7ff440c54206591dc44d00330858ffa6100c942f
George Benjamin <msdcconnect@gmail.com>
2020-02-29 22:13:40 +0100

    style interests page

--- src/components/Onboarding/Interests.vue
+++ src/components/Onboarding/Interests.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h1>Here are some topics you can follow..</h1>
-    <TopicCard v-for="topic in topics" :key="topic.slug" :topic="topic" />
+  <div class="bg-gray-200 py-20 px-3">
+    <h1 class="text-xl font-semibold pb-4">Here are some topics you can follow..</h1>
+    <div class="topics flex flex-wrap">
+      <TopicCard v-for="topic in topics" :key="topic.slug" :topic="topic" />
+    </div>
   </div>
 </template>
 
@@ -9,28 +11,20 @@
 import { Component, Vue} from 'vue-property-decorator'; 
 import { Topic } from '../../types';
 import TopicCard from '../TopicCard.vue';
+import { getModule } from 'vuex-module-decorators';
+import { getTopics } from '@/api/topics';
 @Component({
   components: { 
     TopicCard,
   }
 })
 export default class Interests extends Vue { 
-  topics: Topic[] = [
-    {
-      id: 1,
-      name: 'Tech',
-      imageUrl: 'https://digitalbarker.com/wp-content/uploads/2019/09/2018-07-10-image-35.jpg',
-      slug: 'tech',
-      followerCount: 30, 
-    },
-    {
-      id: 1,
-      name: 'Finance',
-      imageUrl: 'https://www.wbcsd.org/var/site/storage/images/media/images/finance_img/25992-1-eng-GB/finance_img_i1140.jpg',
-      slug: 'finance',
-      followerCount: 50, 
-    },
-  ]
+  topics: Topic[] = [];
+
+  async created() {
+    const topics = await getTopics();
+    this.topics = topics;
+  }
  }
 </script>
 
